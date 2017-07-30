62801303db60c3da5bc863acf735d015dd83b4c5
PingPongCode <thanhnhuanqn@gmail.com>
2017-07-30 15:19:17 +0700

    Add Name to Artist

--- GigHub/Models/ApplicationUser.cs
+++ GigHub/Models/ApplicationUser.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Claims;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -7,6 +8,9 @@ namespace GigHub.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+        
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
